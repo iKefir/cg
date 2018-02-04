8f43d6c9aeb92e96b72c924639e2a9c3915aafbb
iKefir <danshk@mail.ru>
2018-02-04 20:13:12 +0300

    finished methods description

--- 21_half_plane_intersect/visualisation.py
+++ 21_half_plane_intersect/visualisation.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+from mpl_toolkits.mplot3d import Axes3D
+from pyhull.convex_hull import ConvexHull
 from geomlib import *
 
 
@@ -33,91 +36,91 @@ def draw_dual_transform():
     ylb = 0
     yrb = 10
 
-    X = np.array([-4, -1, -1, -0.25, 1, 0.5])
-    Y = np.array([5, -5, 0, -3, -10, -10])
-    clr = np.array(["red", "yellow", "brown", "green", "blue", "grey"])
+    A = np.array([-4, -1, -1, -0.25,   1, 0.5])
+    B = np.array([1,   1,  1,     1,   1,   1])
+    C = np.array([5,  -5,  0,    -3, -10, -10])
+    CLR = np.array(["red", "yellow", "brown", "green", "blue", "grey"])
     HP = []
 
+    X = A/B
+    Y = C/B
+
+    ch = ConvexHull(np.transpose([X, Y]))
+
     ax = plt.subplot(1, 2, 1)
-    for x, y, c in zip(X, Y, clr):
-        HP.append(HalfPlane(x, 1, y))
-        HP[len(HP) - 1].draw(bclr=c, left_b=xlb, right_b=xrb)
+    for a, b, c, clr in zip(A, B, C, CLR):
+        HP.append(HalfPlane(a, b, c))
+        HP[len(HP) - 1].draw(bclr=clr, left_b=xlb, right_b=xrb)
 
-    bottomHP = HalfPlane(0, -1, 0)
-    p0 = bottomHP.intersect(HP[0])
-    p1 = HP[0].intersect(HP[2])
-    p2 = HP[2].intersect(HP[3])
-    p3 = HP[3].intersect(HP[4])
-    p4 = HP[4].intersect(bottomHP)
-    ax.plot([p0.x, p1.x], [p0.y, p1.y], "black", linewidth=3)
-    ax.plot([p1.x, p2.x], [p1.y, p2.y], "black", linewidth=3)
-    ax.plot([p2.x, p3.x], [p2.y, p3.y], "black", linewidth=3)
-    ax.plot([p3.x, p4.x], [p3.y, p4.y], "black", linewidth=3)
-
-    p_point = Point(10/3, 10/3+5)
-    ax.plot(p_point.x, p_point.y, "black", marker="o")
-    ax.text(p_point.x + 0.2, p_point.y, "p")
+    ax.text(3, 2, "$\mathcal{LE}$")
 
     ax.axis([xlb, xrb, ylb, yrb])
 
-    ax = plt.subplot(1, 2, 2)
+    bx = plt.subplot(1, 2, 2)
 
-    ax.plot([-4, -1], [5, 0], "black", linewidth=3)
-    ax.plot([-1, -0.25], [0, -3], "black", linewidth=3)
-    ax.plot([-0.25, 1], [-3, -10], "black", linewidth=3)
+    bx.text(-2, 2, "$\mathcal{UH}$")
 
-    p_line = Line(p_point.x, 1, p_point.y)
-    p_line.draw(left_b=-4, right_b=1, linestyle="--", lwidth=1)
-    ax.text(-2.5, 0, "p*")
+    leftX = 500
+    rightX = -500
+    leftInd = 0
+    rightInd = 0
 
-    for x, y, c in zip(X, Y, clr):
-        ax.plot(x, y, color=c, marker="o")
+    bottomHP = HalfPlane(0, -1, 0)
+
+    P = []
+
+    for v in ch.vertices:
+        if X[v[0]] > X[v[1]]:
+            P.append(HP[v[0]].intersect(HP[v[1]]))
+            bx.plot(X[v], Y[v], color = "black", linewidth = 3)
+            if X[v[0]] > rightX:
+                rightX = X[v[0]]
+                rightInd = v[0]
+            if X[v[1]] < leftX:
+                leftX = X[v[1]]
+                leftInd = v[1]
+
+    P.append(HP[leftInd].intersect(bottomHP))
+    P.append(HP[rightInd].intersect(bottomHP))
+    P.sort(key=lambda p: p.x)
+
+    for i in range(len(P) - 1):
+        ax.plot([P[i].x, P[i+1].x], [P[i].y, P[i+1].y], color = "black", linewidth = 3)
+
+    for x, y, clr in zip(X, Y, CLR):
+        bx.plot(x, y, color=clr, marker="o")
 
     plt.show()
 
 
-def draw_second_dual_transform():
+def execute_draw(A, B, C, CLR):
     xlb = 0
     xrb = 10
     ylb = 0
     yrb = 10
 
-    X = np.array([-4, -1, -1, -0.25, 1, 0.5])
-    Y = np.array([5, -5, 0, -3, -10, -10])
-    clr = np.array(["red", "yellow", "brown", "green", "blue", "grey", "violet"])
     HP = []
 
     ax = plt.subplot(1, 3, 1)
 
-    for x, y, c in zip(X, Y, clr):
-        HP.append(HalfPlane(x, 1, y))
-        HP[len(HP) - 1].draw(bclr=c, left_b=xlb, right_b=xrb)
-
-    HP.append(HalfPlane(0.15, -1, 0))
-    HP[len(HP) - 1].draw(bclr=clr[len(HP) - 1], left_b=xlb, right_b=xrb)
-
-    p0 = HP[6].intersect(HP[0])
-    p1 = HP[0].intersect(HP[2])
-    p2 = HP[2].intersect(HP[3])
-    p3 = HP[3].intersect(HP[4])
-    p4 = HP[4].intersect(HP[6])
-    ax.plot([p0.x, p1.x], [p0.y, p1.y], "black", linewidth=3)
-    ax.plot([p1.x, p2.x], [p1.y, p2.y], "black", linewidth=3)
-    ax.plot([p2.x, p3.x], [p2.y, p3.y], "black", linewidth=3)
-    ax.plot([p3.x, p4.x], [p3.y, p4.y], "black", linewidth=3)
-    ax.plot([p4.x, p0.x], [p4.y, p0.y], "black", linewidth=3)
+    for a, b, c, clr in zip(A, B, C, CLR):
+        HP.append(HalfPlane(a, b, c))
+        HP[len(HP) - 1].draw(bclr=clr, left_b=xlb, right_b=xrb)
 
     move_point = Point(4, 2)
     ax.plot(move_point.x, move_point.y, "black", marker="o")
     ax.text(move_point.x, move_point.y + 0.2, "a")
 
-    p_point = Point(10 / 3, 10 / 3 + 5)
-    ax.plot(p_point.x, p_point.y, "black", marker="o")
-    ax.text(p_point.x + 0.2, p_point.y, "p")
-
     ax.axis([xlb, xrb, ylb, yrb])
 
     bx = plt.subplot(1, 3, 2)
+
+    C = C + A*4 + B*2
+
+    X = A / C
+    Y = B / C
+
+    ch = ConvexHull(np.transpose([X, Y]))
 
     xlb -= move_point.x
     xrb -= move_point.x
@@ -126,24 +129,7 @@ def draw_second_dual_transform():
 
     for i in range(len(HP)):
         HP[i].move(4, 2)
-        HP[i].draw(bclr=clr[i], left_b=xlb, right_b=xrb)
-
-    p0 = HP[6].intersect(HP[0])
-    p1 = HP[0].intersect(HP[2])
-    p2 = HP[2].intersect(HP[3])
-    p3 = HP[3].intersect(HP[4])
-    p4 = HP[4].intersect(HP[6])
-
-    bx.plot([p0.x, p1.x], [p0.y, p1.y], "black", linewidth=3)
-    bx.plot([p1.x, p2.x], [p1.y, p2.y], "black", linewidth=3)
-    bx.plot([p2.x, p3.x], [p2.y, p3.y], "black", linewidth=3)
-    bx.plot([p3.x, p4.x], [p3.y, p4.y], "black", linewidth=3)
-    bx.plot([p4.x, p0.x], [p4.y, p0.y], "black", linewidth=3)
-
-    p_point.x -= move_point.x
-    p_point.y -= move_point.y
-    bx.plot(p_point.x, p_point.y, "black", marker="o")
-    bx.text(p_point.x + 0.2, p_point.y, "p")
+        HP[i].draw(bclr=CLR[i], left_b=xlb, right_b=xrb)
 
     bx.plot(0, 0, "black", marker="o")
     bx.text(0, 0.2, "a")
@@ -154,30 +140,96 @@ def draw_second_dual_transform():
 
     P = []
 
-    xlb = 100
-    xrb = -100
+    MP = {}
 
-    for i in range(len(HP)):
-        point = Point(HP[i].a / HP[i].c, HP[i].b / HP[i].c)
-        P.append(point)
-        if point.x < xlb:
-            xlb = point.x
-        if point.x > xrb:
-            xrb = point.x
+    for v in ch.vertices:
+        MP[v[0]] = v[1]
+        cx.plot(X[v], Y[v], color = "black", linewidth = 3)
 
-    cx.plot([P[6].x, P[0].x], [P[6].y, P[0].y], "black", linewidth=3)
-    cx.plot([P[0].x, P[2].x], [P[0].y, P[2].y], "black", linewidth=3)
-    cx.plot([P[2].x, P[3].x], [P[2].y, P[3].y], "black", linewidth=3)
-    cx.plot([P[3].x, P[4].x], [P[3].y, P[4].y], "black", linewidth=3)
-    cx.plot([P[4].x, P[6].x], [P[4].y, P[6].y], "black", linewidth=3)
+    ind = ch.vertices[0][0]
 
-    p_line = Line(-p_point.x, -p_point.y, -1)
-    p_line.draw(left_b=xlb, right_b=xrb, linestyle="--", lwidth=1)
-    cx.text(0, -0.12, "p*")
+    for i in range(len(ch.vertices)):
+        P.append(HP[ind].intersect(HP[MP[ind]]))
+        ind = MP[ind]
 
-    for i in range(len(P)):
-        cx.plot(P[i].x, P[i].y, color=clr[i], marker="o")
+    P.append(Point(P[0].x, P[0].y))
+
+    for i in range(len(P) - 1):
+        bx.plot([P[i].x, P[i+1].x], [P[i].y, P[i+1].y], color = "black", linewidth = 3)
+
+    for p in P:
+        p.x += 4
+        p.y += 2
+
+    for i in range(len(P) - 1):
+        ax.plot([P[i].x, P[i+1].x], [P[i].y, P[i+1].y], color = "black", linewidth = 3)
+
+    for x, y, clr in zip(X, Y, CLR):
+        cx.plot(x, y, color=clr, marker="o")
 
     cx.plot(0, 0, "black", marker="o")
+
+    plt.show()
+
+
+def draw_second_dual_transform():
+    A = np.array([-4, -1, -1, -0.25,   1, 0.5, 0.15])
+    B = np.array([1,   1,  1,     1,   1,   1,   -1])
+    C = np.array([5,  -5,  0,    -3, -10, -10,    0])
+    CLR = np.array(["red", "yellow", "brown", "green", "blue", "grey", "violet"])
+
+    execute_draw(A, B, C, CLR)
+
+
+def draw_unbounded():
+    A = np.array([-4, -1, -1, -0.25,   1, 0.5])
+    B = np.array([1,   1,  1,     1,   1,   1])
+    C = np.array([5,  -5,  0,    -3, -10, -10])
+    CLR = np.array(["red", "yellow", "brown", "green", "blue", "grey"])
+
+    execute_draw(A, B, C, CLR)
+
+
+def draw_third_dual_transform():
+    xlb = 0
+    xrb = 10.1
+    ylb = 0
+    yrb = 10.1
+    step = 2
+
+    A = np.array([-4, -1, -1, -0.25,   1, 0.5, 0.15, 0])
+    B = np.array([1,   1,  1,     1,   1,   1,   -1, 0])
+    C = np.array([5,  -5,  0,    -3, -10, -10,    0, 0])
+    CLR = np.array(["red", "yellow", "brown", "green", "blue", "grey", "violet", "black"])
+
+    X = A / -1
+    Y = B / -1
+    Z = C / -1
+
+    ch = ConvexHull(np.transpose([X, Y, Z]))
+
+    fig = plt.figure()
+    # ax = fig.add_subplot(121, projection='3d')
+    #
+    # ax.axes([xlb, xrb, ylb, yrb])
+    #
+    # XS = np.arange(xlb, xrb, step = 1)
+    # YS = np.arange(ylb, yrb, step = 1)
+    # XS, YS = np.meshgrid(XS, YS)
+    #
+    # for i in range(len(A)):
+    #     ZS = A[i]*XS + B[i]*YS + C[i]
+    #     ax.plot_surface(XS, YS, ZS, linewidth = 0, color=CLR[i], antialiased = False)
+
+    bx = fig.add_subplot(111, projection='3d')
+
+    bx.scatter(X, Y, Z, c=CLR, marker="o", alpha=1)
+
+    triangles = []
+    for v in ch.vertices:
+        if v[0] == 7 or v[1] == 7 or v[2] == 7:
+            triangles.append(v)
+
+    bx.plot_trisurf(X, Y, Z, alpha=0.1, triangles=triangles)
 
     plt.show()
